064e3a40214591b583bc0e050b952a0f339dd66e
王文壮 <1020304029@qq.com>
2017-06-15 11:44:10 +0800

    【更新】修改身份证号码验证规则

--- ZKCommon/Extension/String-Extension.swift
+++ ZKCommon/Extension/String-Extension.swift
@@ -40,102 +40,32 @@ public extension String {
     }
     //MARK: 是否是身份证号
     public func isIDCardNumber() -> Bool{
-        let value = self.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        var length = 0
-        if value == ""{
-            return false
-        }else{
-            length = value.characters.count
-            if length != 15 && length != 18{
-                return false
-            }
-        }
-        
-        //省份代码
-        let arearsArray = ["11","12", "13", "14",  "15", "21",  "22", "23",  "31", "32",  "33", "34",  "35", "36",  "37", "41",  "42", "43",  "44", "45",  "46", "50",  "51", "52",  "53", "54",  "61", "62",  "63", "64",  "65", "71",  "81", "82",  "91"]
-        let valueStart2 = (value as NSString).substring(to: 2)
-        var arareFlag = false
-        if arearsArray.contains(valueStart2){
-            arareFlag = true
-        }
-        if !arareFlag{
+        if self.characters.count != 18 {
+            // 非18位
             return false
         }
-        var regularExpression = NSRegularExpression()
-        
-        var numberofMatch = Int()
-        var year = 0
-        switch (length){
-        case 15:
-            year = Int((value as NSString).substring(with: NSRange(location: 6,length: 2)))!
-            if year % 4 == 0 || (year % 100 == 0 && year % 4 == 0) {
-                do {
-                    regularExpression = try NSRegularExpression(pattern: "^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}$", options: .caseInsensitive) //检测出生日期的合法性
-                } catch {
-                }
-            } else {
-                do {
-                    regularExpression =  try NSRegularExpression(pattern: "^[1-9][0-9]{5}[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}$", options: .caseInsensitive) //检测出生日期的合法性
-                } catch {}
-            }
-            numberofMatch = regularExpression.numberOfMatches(in: value, options:NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, value.characters.count))
-            
-            if (numberofMatch > 0) {
-                return true
-            } else {
-                return false
-            }
-        case 18:
-            year = Int((value as NSString).substring(with: NSRange(location: 6,length: 4)))!
-            if year % 4 == 0 || (year % 100 == 0 && year % 4 == 0){
-                do {
-                    regularExpression = try NSRegularExpression(pattern: "^[1-9][0-9]{5}19[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|[1-2][0-9]))[0-9]{3}[0-9Xx]$", options: .caseInsensitive) //检测出生日期的合法性
-                    
-                } catch {
-                    
-                }
-            } else {
-                do {
-                    regularExpression =  try NSRegularExpression(pattern: "^[1-9][0-9]{5}19[0-9]{2}((01|03|05|07|08|10|12)(0[1-9]|[1-2][0-9]|3[0-1])|(04|06|09|11)(0[1-9]|[1-2][0-9]|30)|02(0[1-9]|1[0-9]|2[0-8]))[0-9]{3}[0-9Xx]$", options: .caseInsensitive) //检测出生日期的合法性
-                } catch {}
-            }
-            
-            numberofMatch = regularExpression.numberOfMatches(in: value, options: NSRegularExpression.MatchingOptions.reportProgress, range: NSMakeRange(0, value.characters.count))
-            
-            if (numberofMatch > 0) {
-                let s =
-                    (Int((value as NSString).substring(with: NSRange(location:0,length:1)))! +
-                        Int((value as NSString).substring(with: NSRange(location:10,length:1)))!) * 7 +
-                        (Int((value as NSString).substring(with: NSRange(location:1,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:11,length:1)))!) * 9 +
-                        (Int((value as NSString).substring(with: NSRange(location:2,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:12,length:1)))!) * 10 +
-                        (Int((value as NSString).substring(with: NSRange(location:3,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:13,length:1)))!) * 5 +
-                        (Int((value as NSString).substring(with: NSRange(location:4,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:14,length:1)))!) * 8 +
-                        (Int((value as NSString).substring(with: NSRange(location:5,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:15,length:1)))!) * 4 +
-                        (Int((value as NSString).substring(with: NSRange(location:6,length:1)))! +
-                            Int((value as NSString).substring(with: NSRange(location:16,length:1)))!) *  2 +
-                        Int((value as NSString).substring(with: NSRange(location:7,length:1)))! * 1 +
-                        Int((value as NSString).substring(with: NSRange(location:8,length:1)))! * 6 +
-                        Int((value as NSString).substring(with: NSRange(location:9,length:1)))! * 3
-                let Y = s % 11
-                var M = "F"
-                let JYM = "10X98765432"
-                
-                M = (JYM as NSString).substring(with: NSRange(location:Y,length:1))
-                if M == (value as NSString).substring(with: NSRange(location:17,length:1)).uppercased() {
-                    return true
-                } else { return false }
-            } else {
-                return false
-            }
-        default:
+        let tmp = self as NSString
+        let number = tmp.substring(with: NSMakeRange(0, 17)) as String
+        print(number)
+        if !number.isMatch("^\\d+$") {
+            // 格式错误，非数字
             return false
         }
+        let last = (tmp.substring(with: NSMakeRange(17, 1)) as String).lowercased()
+        print(last)
+        if !last.isMatch("^(\\d|x){1}$") {
+            // 最后一位格式错误
+            return false
+        }
+        let dateStr = tmp.substring(with: NSMakeRange(6, 8)) as String
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        guard let date = dateFormatter.date(from: dateStr) else {
+            // 日期格式错误
+            return false
+        }
+        print(dateFormatter.string(from: date))
+        return true
     }
     //MARK: 替换指定位置身份证号
     public func replaceIDCardNumber(start: Int, length: Int, newValue: String) -> String {
