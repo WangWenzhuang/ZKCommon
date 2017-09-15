994d31d93245f909881217abbb70841a6eb358ee
王文壮 <1020304029@qq.com>
2017-09-15 10:04:49 +0800

    【重构】重构代码

--- ZKCommon/Extension/UIViewController-Extension.swift
+++ ZKCommon/Extension/UIViewController-Extension.swift
@@ -8,19 +8,19 @@
 
 extension UIViewController {
     //MARK: 导航栏高度
-    var navigationBarHeight: CGFloat {
+    public var navigationBarHeight: CGFloat {
         get {
             return self.navigationController?.navigationBar.frame.size.height ?? 0
         }
     }
     //MARK: 导航栏和状态栏高度
-    var navigationBarHeightAndstatusBarHeight: CGFloat {
+    public var navigationBarHeightAndstatusBarHeight: CGFloat {
         get {
             return self.navigationBarHeight + ZKDevice.statusBarHeight
         }
     }
     //MARK: tabBar 高度
-    var tabBarHeight: CGFloat {
+    public var tabBarHeight: CGFloat {
         get {
             return self.tabBarController?.tabBar.height ?? 0
         }
