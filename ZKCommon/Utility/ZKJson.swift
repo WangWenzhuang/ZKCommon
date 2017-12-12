9dce9ac81d14b0f14035cc6aca41714ed2c31659
王文壮 <1020304029@qq.com>
2017-12-12 16:00:20 +0800

    【重构】解决bug

--- ZKCommon/Utility/ZKJson.swift
+++ ZKCommon/Utility/ZKJson.swift
@@ -9,7 +9,7 @@
 import ZKLog
 
 public final class ZKJson {
-    public func json(_ object: Any) -> String {
+    public static func json(_ object: Any) -> String {
         if (!JSONSerialization.isValidJSONObject(object)) {
             ZKLog.error("无法解析 JSON")
             return ""
